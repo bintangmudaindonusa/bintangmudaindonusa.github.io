eda4795783f58c65d74f66f534ea93b254d062b6
PT. Bintang Muda Indonusa <166154536+bintangmudaindonusa@users.noreply.github.com>
2024-04-16 04:47:52 +0800

    Update version web script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -67,7 +67,7 @@ var dataInstagram = "bintang_muda_indonusa";
 var dataTiktok = "bintangmudaindonusa";
 var dataYoutube = "bintangmudaindonusa";
 var versiApp = "1.1.24"; // nomor versi : nomor batch : tahun
-var versiWeb = "Developer"; // Developer / Online
+var versiWeb = "Online"; // Developer / Online
 // let pathWeb = "/bintang-muda-indonusa/";
 let pathWeb = "/";
 
